feb66d4a9d196370f6e499ead64dbadc13eb0634
olegmaroseev <olegmaroseev@gmail.com>
2016-05-29 01:13:29 +0300

    Мелкие правки по коду

--- DataCreation.hs
+++ DataCreation.hs
@@ -7,37 +7,27 @@ module DataCreation where
 import Language.Haskell.TH
 import Data.Aeson
 import Data.Maybe
-import Data.HashMap.Strict as StrHash
+import qualified Data.HashMap.Strict as StrHash
 import Data.Text
 import Data.Char
 import Data.Generics
 import Language.Haskell.TH.Syntax
 import Data.Vector
-import Control.Monad.State
+import Control.Monad.State        as MState
 import qualified Data.Foldable    as FB
 import qualified Data.Map         as M
-import Data.List
+import qualified Data.List        as DList
 
 personJSON =  fromJust $ decode $ "{\"name\":\"Joe\",\"age\":25,\"avg\":4,\"arr\" : [1,2,3]}" :: Value
 
 personJSON2 =  fromJust $ decode $ "{\"name\":\"Joe\",\"age\":25,\"avg\":4,\"arra\" : {\"fg\" : \"qwerty\"}}" :: Value
 
-arrJs = fromJust $ decode $ "{\"fg\" : \"qwerty\"}" :: Maybe Value
+arrJs = fromJust $ decode $ "{\"fg\" : \"qwerty\"}" ::Value
 
-compJSON = fromJust $ decode $ "{\"name\":\"Joe\",\"age\":{\"foo\": {\"r\" : 12}}}" :: Maybe Value
+compJSON = fromJust $ decode $ "{\"name\":\"Joe\",\"age\":{\"foo\": {\"r\" : 12}}}" :: Value
 
-foldrWithKeyM :: (Monad m) => (k -> a -> b -> m b) -> b -> M.Map k a -> m b
-foldrWithKeyM f b = FB.foldrM (uncurry f) b . M.toList
-{-
-createData name' json' =
-           DataD
-                []
-                (mkName name')
-                []
-                [ RecC (mkName name') (mka $ json') ]
-                [mkName "Show", mkName "Eq"]
--}
---toHashMap :: Value -> Object
+--получение Hashmap из объекта
+toHashMap :: Value -> Object
 toHashMap (Object obj) = obj
 
 --проверка на то, что Value является Object
@@ -50,21 +40,28 @@ isComp:: Value -> Bool
 isComp (Object obj) = StrHash.foldl' (\pred elem -> if (isObject elem) then True
                                                   || pred else pred) False (obj)
 
+--получение ключей HasMap
 getMapKeys :: Maybe Value -> [Text]
-getMapKeys map' = foldlWithKey' (\list' key' val' -> if (isObject val')
+getMapKeys map' = StrHash.foldlWithKey' (\list' key' val' -> if (isObject val')
                           then key' : list'
                           else key' : list') [] (toHashMap $ fromJust $ map')
 
+--переход первой буквы из строчной в заглавную
 firstLetterToUpper :: String -> String
-firstLetterToUpper fieldName = (Data.Char.toUpper $ Prelude.head fieldName)
-                                                     : (Prelude.tail fieldName)
+firstLetterToUpper fieldName = (Data.Char.toUpper $ DList.head fieldName)
+                                                     : (DList.tail fieldName)
 
---makeListVarStrictType :: [Text] -> [VarStrictType]
---makeListVarStrictType fNames = foldl (\list' val' -> (mkName) : list') [] fNames
+--создание списка типов для полей
+makeListVarStrictType :: Foldable t => t a -> [String -> Name]
+makeListVarStrictType fNames = DList.foldl (\list' val' -> (mkName) : list')
+                                                                       [] fNames
 
+--извлечение содержимого из типа
+getContentFromType :: Type -> Name
 getContentFromType (ConT val') = val'
 
---mkValType :: Value -> Type
+--создание типа для определения синтаксиса полей
+mkValType :: Value -> String -> Type
 mkValType (Number val') name'= ConT (mkName "Float")
 mkValType (String val') name' = ConT (mkName "String")
 mkValType (Bool val') name' = ConT (mkName "Bool")
@@ -72,51 +69,57 @@ mkValType (Array val') name' = AppT (ListT)
                                     (mkValType (Data.Vector.head val') name')
 mkValType (Object val') name' = ConT (mkName $ firstLetterToUpper name')
 
+--количество вложенных объектов
 numOfInsertedObjects:: Value -> Int
-numOfInsertedObjects (Object obj) = foldlWithKey' (\acc' key' val' ->
+numOfInsertedObjects (Object obj) = StrHash.foldlWithKey' (\acc' key' val' ->
          if isComp (Object obj)
            then acc' + 1 + numOfInsertedObjects val'
            else acc' + numOfInsertedObjects val') 0 obj
-
 numOfInsertedObjects _ = 0
 
-mapKeys' =  (foldlWithKey'
+mapKeys' =  (StrHash.foldlWithKey'
     (\map' key' value' -> StrHash.insert (Data.Text.unpack key') value' map')
     StrHash.empty)
 
-foldlWithKeyM :: (Monad m) => (b -> k -> a -> m b) -> b -> StrHash.HashMap k a -> m b
+foldlWithKeyM :: (Monad m) => (b -> k -> a -> m b) -> b ->
+                                                      StrHash.HashMap k a -> m b
 foldlWithKeyM f b = FB.foldlM f' b . StrHash.toList
   where f' a = uncurry (f a)
 
+--конвертация полей объекта
+convertFields:: MonadState [Dec] m => Value -> m [(Name, Strict, Type)]
 convertFields map' = foldlWithKeyM upd [] (mapKeys' $ toHashMap map')
  where
-    upd list' key' val' 
+    upd list' key' val'
         | isObject val' =
                 do
                   result <- convertFields $ val'
-                  Control.Monad.State.modify ((Prelude.++) [DataD
-                                                             []
-                                                             (mkName $ firstLetterToUpper key')
-                                                             []
-                              [ RecC (mkName $ firstLetterToUpper key')  (result) ]
-                                                          [mkName "Show", mkName "Eq"] ])
-                  (Control.Monad.State.return  (((mkName $  key'), NotStrict, (   mkValType val' key')) : list'))
+                  MState.modify ((DList.++) [DataD
+                          []
+                          (mkName $ firstLetterToUpper key')
+                          []
+                          [ RecC (mkName $ firstLetterToUpper key')  (result) ]
+                          [mkName "Show", mkName "Eq"] ])
+                  (MState.return  (((mkName $  key'), NotStrict,
+                                             (   mkValType val' key')) : list'))
         | otherwise =
                 do
-                  (Control.Monad.State.return (((mkName $  key'), NotStrict, (mkValType val' key') )    : list'))
-                
+                  (MState.return (((mkName $  key'), NotStrict,
+                                            (mkValType val' key') )    : list'))
+
+--конвертация основного объекта
 convertObject:: String -> Value -> State [Dec] ()
 convertObject name' json' = do
     result <- convertFields json'
-    Control.Monad.State.modify $ (Prelude.++) [DataD
+    MState.modify $ (DList.++) [DataD
                 []
                 (mkName name')
                 []
                 [ RecC (mkName name') result ]
                 [mkName "Show", mkName "Eq"] ]
 
+--
 getDataFromJSON::DecsQ
 getDataFromJSON = do
   return $
-          (snd (runState (convertObject "JSONData" personJSON2) $ []) ) 
-
+          (snd (runState (convertObject "JSONData" personJSON2) $ []) )
